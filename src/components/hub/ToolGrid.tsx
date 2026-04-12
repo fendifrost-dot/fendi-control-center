48f5fcecdcd4752e358bdfde3e1c38523871b256
fendifrost-dot <fendifrost@gmail.com>
2026-04-12 14:14:25 -0500

    feat(hub): add ToolGrid responsive grid component

---
+++ src/components/hub/ToolGrid.tsx
@@ -0,0 +1,12 @@
+import { TOOL_REGISTRY } from "./toolRegistry";
+import { ToolTile } from "./ToolTile";
+
+export function ToolGrid() {
+  return (
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+      {TOOL_REGISTRY.map((tool) => (
+        <ToolTile key={tool.id} tool={tool} />
+      ))}
+    </div>
+  );
+}
